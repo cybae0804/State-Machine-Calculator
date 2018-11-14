3be7311b2e5779e1db820a59e468570205169a3f
Cy Bae <cybae0804@gmail.com>
2018-11-13 17:27:24 -0800

    calculator v0.5 - Cy Bae

---
+++ main.js
@@ -0,0 +1,181 @@
+$(document).ready(init);
+
+function init() {
+    $('button').click(handleClick);
+    $('.screenText').text(0);
+    initializeKeyPress();
+}
+
+function handleClick() {
+    let input = $(this).text().trim();
+    if ((!isNaN(input)) || input === '.'){
+        stateObj.digit(input);
+    } else if (input === '+' || input === '-' || input === '÷' || input === '×'){
+        stateObj.operation(input);
+    } else if (input === '='){
+        stateObj.eval();
+    } else if (input === 'C'){
+        stateObj.reset();
+    } else if (input === 'CE'){
+        stateObj.clearEntry();
+    }
+}
+
+const stateObj = {
+    current: '0',
+    operator: null,
+    operand: '0',
+    history: [0],
+    state: 'num',
+    digit: function(input){
+        switch(this.state){
+            case 'num':
+                if(input === '.' && this.current[this.current.length-1] === '.'){
+                    break;
+                }
+                this.current = this.current + String(input);
+                this.updateDisplay(this.current);
+                break;
+            case 'operator':
+                this.state = 'operand';
+                this.operand = String(0);
+                this.operand = this.operand + String(input);
+                this.updateDisplay(this.operand);
+                break;
+            case 'operand':
+                if(input === '.' && this.operand[this.operand.length-1] === '.'){
+                    break;
+                }
+                this.operand = this.operand + String(input);
+                this.updateDisplay(this.operand);
+            case 'eval':
+                stateObj.reset();
+                stateObj.digit(input);
+                break;
+        }
+        
+    },
+    operation: function (input) {
+        switch(this.state){
+            case 'num':
+                this.state = 'operator';
+                this.operator = input; 
+                break;
+            case 'operator':
+                this.operator = input;
+                break;
+            case 'operand':
+                this.state = 'operator';
+                this.doMath(this.current, this.operator, this.operand);
+                this.operator = input;
+                this.operand = 0;
+                this.updateDisplay(this.current);
+                break;
+            case 'eval':
+                this.state = 'operator';
+                this.operator = input;
+                break;
+        }
+    },
+    eval: function() {
+        switch(this.state){
+            case 'num':
+                break;
+            case 'operator':
+                this.doMath(this.current, this.operator, this.current);
+                break;
+            case 'operand':
+                this.state = 'eval';
+                this.doMath(this.current, this.operator, this.operand);
+                break;
+            case 'eval':
+                this.doMath(this.current, this.operator, this.operand);
+                break;
+        }
+        this.updateDisplay(this.current);
+    },
+    doMath: function(op1, operator, op2) {
+        let value;
+        op1 = Number(op1);
+        op2 = Number(op2);
+        switch(operator){
+            case '+':
+                value = op1 + op2;
+                break;
+            case '-':
+                value = op1 - op2;
+                break;
+            case '÷':
+                if (op2 === 0){
+                    console.log('div by 0 error');
+                }
+                value = op1 / op2;
+                break;
+            case '×':
+                value = op1 * op2;
+                break;
+        }
+        this.history.push(value);
+        this.current = String(value);
+    },
+    updateDisplay: function(value){
+        $('.screenText').text(Number(value));
+    },
+    reset: function(){
+        this.current = '0';
+        this.operator = null;
+        this.operand = '0';
+        this.history = [];
+        this.state = 'num';
+        this.updateDisplay('0');
+    },
+    clearEntry: function(){
+        if (this.history.length > 1){
+            this.history.pop();
+            let value = this.history[this.history.length-1];
+            this.updateDisplay(value);
+            this.current = value;
+        }
+    }
+};
+
+function initializeKeyPress(){
+    document.onkeydown = function(evt) {
+        if (evt.keyCode === 13 || evt.keyCode === 187){ //enter/equals
+            stateObj.eval();
+        } else if (evt.keyCode === 48 || evt.keyCode === 96){ //0
+            stateObj.digit(0);
+        } else if (evt.keyCode === 49 || evt.keyCode === 97){ //1
+            stateObj.digit(1);
+        } else if (evt.keyCode === 50 || evt.keyCode === 98){ //2
+            stateObj.digit(2);
+        } else if (evt.keyCode === 51 || evt.keyCode === 99){ //3
+            stateObj.digit(3);
+        } else if (evt.keyCode === 52 || evt.keyCode === 100){ //4
+            stateObj.digit(4);
+        } else if (evt.keyCode === 53 || evt.keyCode === 101){ //5
+            stateObj.digit(5);
+        } else if (evt.keyCode === 54 || evt.keyCode === 102){ //6
+            stateObj.digit(6);
+        } else if (evt.keyCode === 55 || evt.keyCode === 103){ //7
+            stateObj.digit(7);
+        } else if (evt.keyCode === 56 || evt.keyCode === 104){ //8
+            stateObj.digit(8);
+        } else if (evt.keyCode === 57 || evt.keyCode === 105){ //9
+            stateObj.digit(9);
+        } else if (evt.keyCode === 106){ //mult
+            stateObj.operation('×');
+        } else if (evt.keyCode === 107){ //add
+            stateObj.operation('+');
+        } else if (evt.keyCode === 109){ //sub
+            stateObj.operation('-');
+        } else if (evt.keyCode === 111){ //div
+            stateObj.operation('÷');
+        } else if (evt.keyCode === 110 || evt.keyCode === 190){ //decimal
+            stateObj.digit('.');
+        } else if (evt.keyCode === 27){
+            stateObj.reset();
+        }
+
+    };
+}
